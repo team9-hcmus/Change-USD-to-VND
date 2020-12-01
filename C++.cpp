89326177be6484ebe430904ac3c9ed2edcb1dbd9
team9-hcmus <75234933+team9-hcmus@users.noreply.github.com>
2020-12-01 14:02:01 +0700

    Create C++.cpp

---
+++ C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "conio.h"
+using namespace std; 
+
+int main()
+{
+	double n;
+	cout << "Nhap vao so tien o dang USD: ";
+	cin >> n;
+	double a = n * 23000;
+
+	cout << "\nSo tien sau khi chuyen sang dang VND la: " << a;
+	_getch();
+	return 0;
+}
